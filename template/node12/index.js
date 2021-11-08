30321f449dcbb4eed7bec3c8e85535944d25265b
Louis G <louis@lenra.me>
2021-11-08 11:38:01 +0100

    feat: add listeners and widgets entrypoint

--- template/node12/index.js
+++ template/node12/index.js
@@ -4,12 +4,9 @@
 
 "use strict"
 
-const fs = require('fs');
 const express = require('express')
 const app = express()
-const listenersHandler = require('../application/listeners/index');
-const uiHandler = require('../application/ui/index');
-const performance = require('perf_hooks').performance;
+const manifest = require('../application/manifest');
 const bodyParser = require('body-parser')
 
 const defaultMaxSize = '100kb' // body-parser default
@@ -37,12 +34,20 @@ if (process.env.RAW_BODY === 'true') {
     app.use(bodyParser.urlencoded({ extended: true }));
 }
 
-const middleware = async (req, res) => {
-    // Checking whether middleware received a Resource or Action request
-    if (req.body.resource) {
-        handleAppResource(req, res);
-    } else {
-        handleAppAction(req, res);
+const entrypoint = async (req, res) => {
+    switch (req.body.call) {
+        case "res":
+            handleAppResource(req, res);
+            break;
+        case "listener":
+            handleListenerCall(req, res);
+            break;
+        case "widget":
+            handleWidgetCall(req, res);
+            break;
+        default:
+            res.status(500).send("Body property 'call' is invalid. Must be 'res' for resources, 'listener' for a listener call or 'widget' for a widget call.");
+            break;
     }
 };
 
@@ -50,39 +55,75 @@ function handleAppResource(req, res) {
     const resources_path = "../application/resources/";
 
     // Checking file extensions according to which ones Flutter can handle
-    if(req.body.resource.match(/.*(\.jpeg|\.jpg|\.png|\.gif|\.webp|\.bmp|\.wbmp)$/)){
-        res.sendFile(req.body.resource, {root: resources_path});
+    if (req.body.resource.match(/.*(\.jpeg|\.jpg|\.png|\.gif|\.webp|\.bmp|\.wbmp)$/)) {
+        res.sendFile(req.body.resource, { root: resources_path });
     } else {
         res.sendStatus(404);
     }
 }
 
-function handleAppAction(req, res) {
-    let uiStartTime;
-    let uiStopTime;
-    let listenersStopTime;
-    let newData = {};
-    let { action, data, props, event } = req.body;
-    let listenersStartTime = process.hrtime.bigint();
-    let possibleFutureRes = listenersHandler(action, data, props, event);
-
-    Promise.resolve(possibleFutureRes)
-        .then(res => {
-            listenersStopTime = process.hrtime.bigint();
-            newData = res;
-            uiStartTime = process.hrtime.bigint();
-            return uiHandler(newData);
-        })
-        .then(newUi => {
-            uiStopTime = process.hrtime.bigint();
-            res.status(200).json({ data: newData, ui: newUi, stats: { listeners: Number(listenersStopTime - listenersStartTime), ui: Number(uiStopTime - uiStartTime) } });
-        })
-        .catch(err => {
-            res.status(500).send(err.toString ? err.toString() : err);
-        });
+/**
+ * expect the body to be :
+ *  {
+ *      calls: [
+ *          {name: "myWidget", data: {"my": "data"}, props: {"my": "props"}}
+ *          ...
+ *      ]
+ *  }
+ * 
+ *  expect the manifest to have the map of all widget functions : 
+ *  {
+ *      "myWidget": myWidget(data, props) {...} 
+ *      ...
+ * }
+ * 
+ * Then apply all the widget call and send the result to the res with 200 status.
+ */
+function handleWidgetCall(req, res) {
+    try {
+        let uiStartTime = process.hrtime.bigint();
+        let { calls } = req.body;
+        let widgets = calls.map(call => manifest.widgets[call.name](call.data, call.props));
+        let uiStopTime = process.hrtime.bigint();
+        res.status(200).json({ widgets: widgets, stats: { ui: Number(uiStopTime - uiStartTime) } });
+    } catch (err) {
+        res.status(500).send(err.toString ? err.toString() : err);
+    }
 }
 
-app.post('/*', middleware);
+
+/**
+ * 
+ *  Expect the body to be like : 
+ *  { 
+ *      "action": "myAction",
+ *      "data": {"my": "data"},
+ *      "props": {"my": "props"},
+ *      "event": {"value": "foo"},
+ *  }
+ * 
+ *  Expect the manifest to have the map of all listeners
+ *  {
+ *      listeners: {
+ *          "myAction": myAction(data, props, event) { ... }
+ *      }
+ *  }
+ */
+function handleListenerCall(req, res) {
+    let listenersStartTime = process.hrtime.bigint();
+    let listenersStopTime;
+    let { action, data, props, event } = req.body;
+    let asyncListenerCall = manifest.listeners[action](data, props, event);
+    Promise.resolve(asyncListenerCall).then(res => {
+        listenersStopTime = process.hrtime.bigint();
+        res.status(200).json({ stats: { listeners: Number(listenersStopTime - listenersStartTime) } });
+    }).catch(err => {
+        res.status(500).send(err.toString ? err.toString() : err);
+    });
+
+}
+
+app.post('/*', entrypoint);
 
 const port = process.env.http_port || 3000;
 
