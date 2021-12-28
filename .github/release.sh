47ad8bb8fb34e95b64d68a216e07e190b26cac8c
Shiipou <shiishii@nocturlab.fr>
2021-12-28 11:24:22 +0100

    ci: new docker tag using regex

--- .github/release.sh
+++ .github/release.sh
@@ -2,31 +2,69 @@
 
 set -xe # Show output on the logs
 
-version="$1" # Get version tag
-version="${version#v}" # remove the first `v` char
+function get_tags {
+  version="$1" # Get version tag
+  DOCKER_IMAGE=$2
+  
+  regex='v([0-9]+.[0-9]+.[0-9]+)-?([a-z]+)?.?([0-9]+)?'
+  
+  if [[ $version =~ $regex ]]; then
+    v="${BASH_REMATCH[1]}"
+    channel="${BASH_REMATCH[2]}"
+    channel_version="${BASH_REMATCH[3]}"
 
-read -r -a DOCKER_IMAGE <<< "${DOCKER_IMAGE}" # Convert string into array of string
+    tag="--tag ${DOCKER_IMAGE}:${version#v}"
 
-tag=("--tag ${DOCKER_IMAGE[0]}:$version" "--tag ${DOCKER_IMAGE[1]}:$version")
-if [[ "$version" != *"-beta."* ]]; then # If the version is a prerelease then don't publish on the short version code
-  tag[0]="${tag[0]} --tag ${DOCKER_IMAGE[0]}:${version%%.*} --tag ${DOCKER_IMAGE[0]}:latest" # Remove all after the first dot (dot included)
-  tag[1]="${tag[1]} --tag ${DOCKER_IMAGE[1]}:${version%%.*} --tag ${DOCKER_IMAGE[1]}:latest" # Remove all after the first dot (dot included)
-fi
+    regex='([0-9]+).([0-9]+).([0-9]+)'
+    if [[ $v =~ $regex ]]; then
+      major=${BASH_REMATCH[1]}
+      minor=${BASH_REMATCH[2]}
+      patch=${BASH_REMATCH[3]}
 
-# build the docker image
+      arr_version=( "${major}" "${major}.${minor}" "${major}.${minor}.${patch}" )
+      if [[ -n "${channel}" ]]; then
+        tag="${tag} --tag ${DOCKER_IMAGE}:${channel}"
+        for i in "${arr_version[@]}"; do
+          tag="${tag} --tag ${DOCKER_IMAGE}:${i}-${channel}"
+        done
+      else
+        tag="--tag ${DOCKER_IMAGE}:latest"
+        for i in "${arr_version[@]}"; do
+          tag="${tag} --tag ${DOCKER_IMAGE}:${i}"
+        done
+      fi
+      return 0
+    else
+      echo "Version '$v' didn't pass Regex '$regex'." 1>&2
+      return 1
+    fi
+  else
+    echo "Version '$version' didn't pass Regex '$regex'." 1>&2
+    return 1
+  fi
+}
+
+VERSION="$1" # Get version tag
+
+read -r -a DOCKER_IMAGES <<< "${DOCKER_IMAGE}" # Convert string into array of string
+
+get_tag "$VERSION" "${DOCKER_IMAGES[0]}"
+
 docker buildx build \
   --output type=image,push=true \
   --platform "amd64,arm64,arm" \
-  ${tag[0]} \
+  ${tag} \
   --build-arg CI=true \
   --cache-from type=local,src=~/.docker-cache/template-node12 \
   --cache-to type=local,dest=~/.docker-cache/template-node12 \
   template/node12
 
+get_tag "$VERSION" "${DOCKER_IMAGES[1]}"
+
 docker buildx build \
   --output type=image,push=true \
   --platform "amd64,arm64,arm" \
-  ${tag[1]} \
+  ${tag} \
   --build-arg CI=true \
   --build-arg TEMPLATE_IMAGE=lenra/template-node12:${version} \
   --cache-from type=local,src=~/.docker-cache/devtools-node12 \
